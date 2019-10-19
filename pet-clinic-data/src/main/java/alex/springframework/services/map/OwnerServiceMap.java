06ce4ea67c3d728ca0245e2f63266670974eeb97
alex <alex.semplinski@gmail.com>
2019-10-19 13:17:09 -0600

    Added MapService files to Git

---
+++ pet-clinic-data/src/main/java/alex/springframework/services/map/OwnerServiceMap.java
@@ -0,0 +1,45 @@
+package alex.springframework.services.map;
+
+import alex.springframework.model.Owner;
+import alex.springframework.services.OwnerService;
+import org.springframework.stereotype.Service;
+import java.util.Set;
+
+
+@Service
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private  Owner owner;
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
+    }
+
+}
---
+++ pet-clinic-data/src/main/java/alex/springframework/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package alex.springframework.services.map;
+
+import alex.springframework.model.Pet;
+import alex.springframework.services.PetService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}

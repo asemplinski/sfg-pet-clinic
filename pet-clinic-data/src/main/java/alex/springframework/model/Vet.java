4b8b0a2580a51b85e7a6b48b379c55b2981dbb47
Alex Semplinski <alex.semplinskI@gmail.com>
2019-10-21 07:57:38 -0600

    Added Vet Specialties and associated to Vet. Closes #25

--- pet-clinic-data/src/main/java/alex/springframework/model/Vet.java
+++ pet-clinic-data/src/main/java/alex/springframework/model/Vet.java
@@ -1,4 +1,16 @@
 package alex.springframework.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<VetSpecialty> specialties;
+
+    public Set<VetSpecialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<VetSpecialty> specialties) {
+        this.specialties = specialties;
+    }
 }
---
+++ pet-clinic-data/src/main/java/alex/springframework/model/VetSpecialty.java
@@ -0,0 +1,14 @@
+package alex.springframework.model;
+
+public class VetSpecialty extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

c711c578e3574004c21f6c710627516a61d8300a
Alex Semplinski <alex.semplinskI@gmail.com>
2019-10-22 08:30:12 -0600

    Implemented Owners JPA. Closes #34

--- pet-clinic-data/src/main/java/alex/springframework/model/Owner.java
+++ pet-clinic-data/src/main/java/alex/springframework/model/Owner.java
@@ -1,14 +1,24 @@
 package alex.springframework.model;
 
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner  extends Person{
 
+    @OneToMany(cascade = CascadeType.ALL)
     private Set<Pet> pets = new HashSet<>();
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
 
     public Set<Pet> getPets() {

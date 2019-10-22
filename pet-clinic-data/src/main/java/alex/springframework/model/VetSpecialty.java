657c6fa87373e99154f247760718074631037872
Alex Semplinski <alex.semplinskI@gmail.com>
2019-10-22 08:35:06 -0600

    Implemented Vets JPA. Closes #35

--- pet-clinic-data/src/main/java/alex/springframework/model/VetSpecialty.java
+++ pet-clinic-data/src/main/java/alex/springframework/model/VetSpecialty.java
@@ -1,7 +1,14 @@
 package alex.springframework.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vet_specialties")
 public class VetSpecialty extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
